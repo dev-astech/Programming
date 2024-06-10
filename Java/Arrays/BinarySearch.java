c0fc3f0c4b02171341aa4e1468f7780b3b18a190
Anas <107978854+dev-astech@users.noreply.github.com>
2024-06-10 20:55:26 +0530

    Binary Search

---
+++ Java/Arrays/BinarySearch.java
@@ -0,0 +1,32 @@
+public class BinarySearch {
+
+    public static int binarySearch(int []numbers, int key) {
+
+        int start = 0;
+        int end = numbers.length-1;
+
+        while (start<=end){
+            int mid = (start+end)/2;
+            if(numbers[mid]==key){
+                return mid;
+            }else if(numbers[mid]<key){
+                start = mid+1;
+            }else {
+                end = mid-1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int []numbers = {2,4,5,8,10,11,13,17,20};
+        int key = 8;
+        int keyIndex = binarySearch(numbers, key);
+        if(keyIndex!=-1){
+            System.out.print(key+" is present at index "+keyIndex);
+        }else {
+            System.out.print(key+" is not present in the array");
+        }
+    }
+}
